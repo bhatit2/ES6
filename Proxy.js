69257a5fa4b4323f8bd84cb81f242442dfea0f7e
tbhatia1912 <tbhatia@tibco.com>
2018-09-25 22:47:54 +0530

    es6 proxy negative indexed array

--- Proxy.js
+++ Proxy.js
@@ -5,14 +5,14 @@
  * var p =  new Proxy(handler, target);
 **/
 
-let target = {
+let targetObj = {
     name : 'name',
     address : 'address'
 };
 
-console.log(target.name); // name
-console.log(target.address); // address
-console.log(target.phone); // undefined
+console.log(targetObj.name); // name
+console.log(targetObj.address); // address
+console.log(targetObj.phone); // undefined
 
 /*When property does not exist we get undefined. I want to return custom message for the properties
  which does not exist in my object. */
@@ -25,8 +25,30 @@ let handler = {
     }
 }
 
-let myProxy = new Proxy(target, handler);
+let myProxy = new Proxy(targetObj, handler);
 
 console.log(myProxy.name); // name
 console.log(myProxy.address); // address
 console.log(myProxy.phone); // undefined
+
+/**Access array values using neative indexes. When accessing array value using negative index,
+ * it should return value from last array index.
+ */
+
+
+let negativeIndexHandler = {
+    get : function(array,index){
+        index = +index;
+        return array[index < 0 ? array.length + index : index];
+    },
+    set : function(array,index, value){
+        index = +index;
+        return array[index < 0 ? array.length + index : index] = value;
+    }
+};
+
+let sampleArray = [1,2,3,4,5,6,7,8,9,10];
+let proxySampleArray = new Proxy(sampleArray,negativeIndexHandler);
+
+console.log(proxySampleArray[-1]); // 10
+console.log(proxySampleArray[-2]); // 9
